0a6f4611c4236b3006d2f5f1f351d8c2832d60a1
Isak Falk <isak.falk@live.se>
2017-08-14 11:59:53 +0100

    Add methods section

--- latex/ucl-latex-thesis-templates/sections/auto/Methods.el
+++ latex/ucl-latex-thesis-templates/sections/auto/Methods.el
@@ -2,6 +2,12 @@
  "Methods"
  (lambda ()
    (LaTeX-add-labels
-    "MethodsCh"))
+    "MethodsCh"
+    "eq:log_likelihood_sum"
+    "eq:ELBO_inequality"
+    "eq:SGVB"
+    "eq:SGVB_analytical_KL"
+    "eq:joint_generative"
+    "eq:translation_SGVB"))
  :latex)
 
